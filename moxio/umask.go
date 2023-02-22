210fd34702b9a286da63c0fe77286d38bc4489e3
Mechiel Lukkien <mechiel@ueber.net>
2023-02-22 23:26:00 +0100

    umask logging line tweak

--- moxio/umask.go
+++ moxio/umask.go
@@ -12,7 +12,7 @@ func CheckUmask() error {
 	old := syscall.Umask(007)
 	syscall.Umask(old)
 	if old&7 != 7 {
-		return fmt.Errorf(`umask must have "7" for world/other, e.g. 007, not current %o`, old)
+		return fmt.Errorf(`umask must have 7 for world/other, e.g. 007, not current %03o`, old)
 	}
 	return nil
 }
