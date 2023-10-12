52e71167a93101cb687c5fcb7683d3c5a8b54ac6
Mechiel Lukkien <mechiel@ueber.net>
2023-10-12 23:15:35 +0200

    rename rfc/index.md to txt, it isn't markdown

--- rfc/fetch.sh
+++ rfc/fetch.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-for number in $(sed -n 's/^\([0-9][0-9]*\)[ \t].*$/\1/p' index.md); do
+for number in $(sed -n 's/^\([0-9][0-9]*\)[ \t].*$/\1/p' index.txt); do
 	if ! test -f "$number"; then
 		curl https://www.rfc-editor.org/rfc/rfc$number.txt >$number || rm $number
 	fi
 done
 
-for name in $(sed -n 's/^\([0-9][0-9]*-eid[0-9][0-9]*\)[ \t].*$/\1/p' index.md); do
+for name in $(sed -n 's/^\([0-9][0-9]*-eid[0-9][0-9]*\)[ \t].*$/\1/p' index.txt); do
 	if ! test -f "$name"; then
 		rfc=$(echo $name | cut -f1 -d-)
 		eid=$(echo $name | cut -f2 -d-)
