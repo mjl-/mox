f33870ba852e9e582f66d51626a9c746ddd8e0ec
Mechiel Lukkien <mechiel@ueber.net>
2025-02-15 12:56:59 +0100

    move the large commands for generating api docs to separate shell script

---
+++ genapidoc.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -eu
+
+# we rewrite some dmarcprt and tlsrpt enums into untyped strings: real-world
+# reports have invalid values, and our loose Go typed strings accept all values,
+# but we don't want the typescript runtime checker to fail on those unrecognized
+# values.
+(cd webadmin && CGO_ENABLED=0 go run ../vendor/github.com/mjl-/sherpadoc/cmd/sherpadoc/*.go -adjust-function-names none -rename 'config Domain ConfigDomain,dmarc Policy DMARCPolicy,mtasts MX STSMX,tlsrptdb Record TLSReportRecord,tlsrptdb SuppressAddress TLSRPTSuppressAddress,dmarcrpt DKIMResult string,dmarcrpt SPFResult string,dmarcrpt SPFDomainScope string,dmarcrpt DMARCResult string,dmarcrpt PolicyOverride string,dmarcrpt Alignment string,dmarcrpt Disposition string,tlsrpt PolicyType string,tlsrpt ResultType string' Admin) >webadmin/api.json
+(cd webaccount && CGO_ENABLED=0 go run ../vendor/github.com/mjl-/sherpadoc/cmd/sherpadoc/*.go -adjust-function-names none Account) >webaccount/api.json
+(cd webmail && CGO_ENABLED=0 go run ../vendor/github.com/mjl-/sherpadoc/cmd/sherpadoc/*.go -adjust-function-names none Webmail) >webmail/api.json
